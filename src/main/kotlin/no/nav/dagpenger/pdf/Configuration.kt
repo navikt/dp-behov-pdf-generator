41b96f713a525e43a3460e3f82be18ec3242748c
André Roaldseth <andre.roaldseth@nav.no>
2026-03-09 19:06:13 +0100

    Fix lint

--- src/main/kotlin/no/nav/dagpenger/pdf/Configuration.kt
+++ src/main/kotlin/no/nav/dagpenger/pdf/Configuration.kt
@@ -8,7 +8,6 @@ import com.natpryce.konfig.overriding
 import com.natpryce.konfig.stringType
 import no.nav.dagpenger.oauth2.CachedOauth2Client
 import no.nav.dagpenger.oauth2.OAuth2Config
-import java.time.Duration
 
 internal object Configuration {
     const val APP_NAME = "dp-behov-pdf-generator"
